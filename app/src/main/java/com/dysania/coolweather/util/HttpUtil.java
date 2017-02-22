1b7759deba805659529c7a0238cbaf82bbbe2256
DysaniazzZ <William.fzy@foxmail.com>
2017-02-22 16:02:56 +0800

    add http and json utils

---
+++ app/src/main/java/com/dysania/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.dysania.coolweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by DysaniazzZ on 22/02/2017.
+ */
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, Callback callback) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        okHttpClient.newCall(request).enqueue(callback);
+    }
+}
