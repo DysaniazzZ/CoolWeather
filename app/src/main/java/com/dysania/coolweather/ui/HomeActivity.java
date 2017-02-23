ef8deb46ceccfdd828edc444448cbc0bce77a132
DysaniazzZ <William.fzy@foxmail.com>
2017-02-23 20:05:01 +0800

    modify status bar color after lollipop

--- app/src/main/java/com/dysania/coolweather/ui/HomeActivity.java
+++ app/src/main/java/com/dysania/coolweather/ui/HomeActivity.java
@@ -2,6 +2,8 @@ package com.dysania.coolweather.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -85,9 +87,18 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatusBarBg();
         setContentView(R.layout.activity_home);
         mUnbinder = ButterKnife.bind(this);
         initData();
+    }
+
+    private void setStatusBarBg() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 
     private void initData() {
